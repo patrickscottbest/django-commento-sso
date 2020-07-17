a0cce2fd3d8170e780279dd301dc128489de9c3b
Patrick Best <patrickscottbest@gmail.com>
2020-07-17 19:18:13 -0400

    removing build dir from github

--- build/lib/django_commento_sso/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from django.conf.urls import url
-from django_commento_sso import views
-
-urlpatterns = [
-    path('commento', views.sso, name='django_commento_sso'),
-]
