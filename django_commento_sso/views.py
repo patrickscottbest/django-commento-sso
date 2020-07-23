0d29e3db76e2e4fcf4327b847d9d9c348e9c450a
Patrick Best <patrickscottbest@gmail.com>
2020-07-23 12:16:27 -0400

    tabs to spaces

--- django_commento_sso/views.py
+++ django_commento_sso/views.py
@@ -16,70 +16,70 @@ def get_user(request):
 
 def sso(request) :
 
-	if settings.SECRET_KEY == None:
-		logger.error("django_commento_sso - COMMENTO_SECRET_KEY not found in settings.py")
-		return HttpResponse(status=204)
+    if settings.SECRET_KEY == None:
+        logger.error("django_commento_sso - COMMENTO_SECRET_KEY not found in settings.py")
+        return HttpResponse(status=204)
 
 
-	if request.GET['token']:
-		logger.debug("token as received %s" % request.GET['token'])
-	else:
-		return HttpResponse(status=204)
+    if request.GET['token']:
+        logger.debug("token as received %s" % request.GET['token'])
+    else:
+        return HttpResponse(status=204)
 
 
-	if request.GET['hmac']:
-		logger.debug("hmac as recevied %s" % request.GET['hmac'])
-	else:
-		return HttpResponse(status=204)
+    if request.GET['hmac']:
+        logger.debug("hmac as recevied %s" % request.GET['hmac'])
+    else:
+        return HttpResponse(status=204)
 
 
-	receivedHmacBytes = bytes.fromhex(request.GET['hmac'])
-	tokenBytes = bytes.fromhex(request.GET['token'])
+    receivedHmacBytes = bytes.fromhex(request.GET['hmac'])
+    tokenBytes = bytes.fromhex(request.GET['token'])
 
-	secretKeyBytes = bytes.fromhex(settings.COMMENTO_SECRET_KEY)
-	expectedHmacBytes = hmac.digest(secretKeyBytes, tokenBytes, 'sha256')
-	print ("expected hmac was %s" % expectedHmacBytes.hex() )
+    secretKeyBytes = bytes.fromhex(settings.COMMENTO_SECRET_KEY)
+    expectedHmacBytes = hmac.digest(secretKeyBytes, tokenBytes, 'sha256')
+    print ("expected hmac was %s" % expectedHmacBytes.hex() )
 
-	if expectedHmacBytes==receivedHmacBytes:
+    if expectedHmacBytes==receivedHmacBytes:
 
-		logger.debug("HMAC comparison of token - match")
+        logger.debug("HMAC comparison of token - match")
 
-		if not request.user.is_authenticated:
-			#return redirect('%s?next=%s%s?hmac=%s&token=%s' % (settings.LOGIN_URL, request.get_host(), request.get_full_path(),request.GET['hmac'],request.GET['token']))
-			return redirect("{0}?next={1}{2}?hmac={3}&token={4}".format(settings.LOGIN_URL, request.get_host(), request.get_full_path(),request.GET['hmac'],request.GET['token']))
-		else:
-			logger.info("django_commento_sso SSO Success for User %s" % request.user.username )
-			payload = {
-			    "token": request.GET['token'],
-			}
-			if not request.user.email:
-				logger.error('django_commento_sso User must have email for Commento SSO')
-				return False
-			else:
-				payload['email']=request.user.email
+        if not request.user.is_authenticated:
+            #return redirect('%s?next=%s%s?hmac=%s&token=%s' % (settings.LOGIN_URL, request.get_host(), request.get_full_path(),request.GET['hmac'],request.GET['token']))
+            return redirect("{0}?next={1}{2}?hmac={3}&token={4}".format(settings.LOGIN_URL, request.get_host(), request.get_full_path(),request.GET['hmac'],request.GET['token']))
+        else:
+            logger.info("django_commento_sso SSO Success for User %s" % request.user.username )
+            payload = {
+                "token": request.GET['token'],
+            }
+            if not request.user.email:
+                logger.error('django_commento_sso User must have email for Commento SSO')
+                return False
+            else:
+                payload['email']=request.user.email
 
-			# try:
-    		# 	uid = request.session['mid']
-    		# 	userobj = User.objects.get(id=uid)
-			# except User.DoesNotExist as e:
-  			# 	logger.error('Error, user does not exist', e)
-			# 	return false
-			# #
-			# thisUser = User.objects.get(username=request.user.username).first()
-			# #avoid simplelazyobject
-			payload['name']=settings.COMMENTO_USER_NAME_FUNCTION(get_user(request))
+            # try:
+            #     uid = request.session['mid']
+            #     userobj = User.objects.get(id=uid)
+            # except User.DoesNotExist as e:
+              #     logger.error('Error, user does not exist', e)
+            #     return false
+            # #
+            # thisUser = User.objects.get(username=request.user.username).first()
+            # #avoid simplelazyobject
+            payload['name']=settings.COMMENTO_USER_NAME_FUNCTION(get_user(request))
 
-			if settings.COMMENTO_USER_LINK_FUNCTION:
-				payload['link']=settings.COMMENTO_USER_LINK_FUNCTION(get_user(request))
-			if settings.COMMENTO_USER_PHOTO_FUNCTION:
-				payload['photo']=settings.COMMENTO_USER_PHOTO_FUNCTION(get_user(request))
+            if settings.COMMENTO_USER_LINK_FUNCTION:
+                payload['link']=settings.COMMENTO_USER_LINK_FUNCTION(get_user(request))
+            if settings.COMMENTO_USER_PHOTO_FUNCTION:
+                payload['photo']=settings.COMMENTO_USER_PHOTO_FUNCTION(get_user(request))
 
-			payloadHMACHex=(hmac.digest( secretKeyBytes, bytes(json.dumps(payload),'utf8'),'sha256')).hex()
-			payloadHex=bytes(json.dumps(payload),'utf8').hex()
+            payloadHMACHex=(hmac.digest( secretKeyBytes, bytes(json.dumps(payload),'utf8'),'sha256')).hex()
+            payloadHex=bytes(json.dumps(payload),'utf8').hex()
 
-			return redirect("https://commento.io/api/oauth/sso/callback?payload={0}&hmac={1}".format(payloadHex, payloadHMACHex))
+            return redirect("https://commento.io/api/oauth/sso/callback?payload={0}&hmac={1}".format(payloadHex, payloadHMACHex))
 
-	else:
-		#spoofed token attempt or error
-		logger.warning("django_commento_sso SSO HMAC comparison - No match")
-		return HttpResponse(status=404)
+    else:
+        #spoofed token attempt or error
+        logger.warning("django_commento_sso SSO HMAC comparison - No match")
+        return HttpResponse(status=404)
